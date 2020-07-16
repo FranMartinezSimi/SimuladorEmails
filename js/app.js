797f1ed4b745b4c992e3aaca8eb6802cd344e626
francisco <jua.martinez@outlook.es>
2020-07-16 17:00:28 -0400

    terminado

--- js/app.js
+++ js/app.js
@@ -3,7 +3,8 @@ const email = document.getElementById('email');
 const asunto = document.getElementById('asunto');
 const mensaje = document.getElementById('mensaje');
 const btnEnviar = document.getElementById('enviar');
-
+const FormularioEnviar = document.getElementById('enviar-mail');
+const resetBtn = document.getElementById('resetBtn');
 //event Listeners
 eventListeners()
 
@@ -14,6 +15,11 @@ function eventListeners(){
     email.addEventListener('blur', validaCampo);
     asunto.addEventListener('blur', validaCampo);
     mensaje.addEventListener('blur', validaCampo);
+
+    //event Listener Boton enviar en el submit 
+    FormularioEnviar.addEventListener('submit', enviarEmail);
+    //resetear Formulario
+    resetBtn.addEventListener('click', resetFormulario);
 };
 
 //funciones
@@ -31,13 +37,35 @@ function validaCampo(){
     if(this.type === 'email'){
         validarEmail(this)
     }
-    let errores = document.querySelector('.error')
+    let errores = document.querySelectorAll('.error')
     if(email.value != '' && asunto.value != '' && asunto.value != ''){
-        if(errores.length)
+        if(errores.lengt === 0)
         btnEnviar.removeAttribute("disabled")
     }
 };
 
+//cuando se envia el correo
+
+function enviarEmail(e) {
+    const spinerGif = document.querySelector('#spinner');
+    spinerGif.style.display ='block'
+
+    //Gif que envia el email
+    const enviado = document.createElement('img');
+    enviado.src ='img/mail.gif';
+    enviado.style.display ='block'
+    setTimeout(function(){
+        spinerGif.style.display='none'
+        document.querySelector('#loaders').appendChild(enviado);
+        setTimeout(function(){
+            enviado.remove();
+            FormularioEnviar.reset();
+        },5000)
+    }, 3000)
+    e.preventDefault(); 
+}
+
+//verifica la longitud del texto en los campos
 function validarLongitud(campo){
     if(campo.value.length > 0){
         campo.style.borderBottomColor ='green';
@@ -45,5 +73,16 @@ function validarLongitud(campo){
     } else {
         campo.style.borderBottomColor ='red';
         campo.classList.add('error');
+    }
+}
+
+function validarEmail(campo){
+    const mensaje = campo.value;
+    if( mensaje.indexOf('@') !== -1){
+        campo.style.borderBottomColor = 'green';
+        campo.classList.remove('error');
+    }else {
+        campo.style.borderBottomColor = 'red';
+        campo.classList.remove('error');
     }
 }
